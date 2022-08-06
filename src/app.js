2f39905a168505e295b97a27a6a45113b842d3dc
Charlytoc <charlyjchaconc@gmail.com>
2022-08-06 02:18:10 +0000

    excuse generator check

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Mi novia ", "Una señora ", "Un motorizado "];
+  let action = ["me dañó ", "se comió ", "me robó "];
+  let what = ["la tarea ", "el teléfono ", "la billetera "];
+  let when = [
+    "en la mañana",
+    "antes de llegar al trabajo",
+    "ayer por la noche"
+  ];
+  let who1 = Math.floor(Math.random() * 3);
+  let action1 = Math.floor(Math.random() * 3);
+  let what1 = Math.floor(Math.random() * 3);
+  let when1 = Math.floor(Math.random() * 3);
+  console.log(who[who1] + action[action1] + what[what1] + when[when1]);
 };
+
+function excusa() {
+  let who = ["Mi novia ", "Una señora ", "Un motorizado "];
+  let action = ["me dañó ", "se comió ", "me robó "];
+  let what = ["la tarea ", "el teléfono ", "la billetera "];
+  let when = [
+    "en la mañana",
+    "antes de llegar al trabajo",
+    "ayer por la noche"
+  ];
+  let who1 = Math.floor(Math.random() * 3);
+  let action1 = Math.floor(Math.random() * 3);
+  let what1 = Math.floor(Math.random() * 3);
+  let when1 = Math.floor(Math.random() * 3);
+  document.getElementById("excusa").innerHTML =
+    who[who1] + action[action1] + what[what1] + when[when1];
+}
